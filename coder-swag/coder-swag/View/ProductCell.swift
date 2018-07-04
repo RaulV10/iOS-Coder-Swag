9906c363c4c62932385882f8603920d0ccb08796
Raul Villarreal <raulvilla0797@gmail.com>
2018-07-03 23:22:54 -0600

    Collection Views

---
+++ coder-swag/coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Raul Ernesto Villarreal Sigala on 7/3/18.
+//  Copyright © 2018 Raul Ernesto Villarreal Sigala. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+
+}
